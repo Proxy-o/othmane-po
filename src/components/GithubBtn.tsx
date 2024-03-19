e03bbd4dbd3ea797b2f18c213f288f39cf1c77ca
proxy-o <aittaleb108@gmail.com>
2024-03-19 01:48:12 +0000

    change git

--- src/components/GithubBtn.tsx
+++ src/components/GithubBtn.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const GithubBtn = () => {
   return (
     <Link
-      href={"https://github.com/taqui-786"}
+      href={"https://github.com/proxy-o"}
       target="blank"
       className="absolute animate-pulse  right-0 bottom-28 flex rounded-l-full justify-center items-center gap-2 z-50 w-fit h-fit p-2 shadow-md  border-y border-r  border-black hover:bg-primary/10 hover:text-white hover:animate-none "
     >
